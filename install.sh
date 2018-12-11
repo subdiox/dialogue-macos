381948e7ae3a8c6ff8543435c5b363374ffce650
subdiox <subdiox@gmail.com>
2018-12-11 14:42:03 +0900

    fix script for high sierra

--- install.sh
+++ install.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
-bin_path=$(dirname $(greadlink -f $0))
+if [ ! `which brew` ]; then
+  echo "[Error] Homebrew is not installed. You can install from https://brew.sh/index_ja"
+  exit
+fi
 
-# 話者認識学習音声の録音用
-bash ${bin_path}/setup/install4recatr.sh
-sudo apt-get install julius -y
-sudo apt-get install osspd -y
+if [ ! `which greadlink` ]; then
+  echo "[Notice] greadlink is not installed. Starting installation..."
+  brew install coreutils
+fi
 
-# 音声認識の julius yomi2voca 用
-sudo apt-get install nkf -y
+if [ ! `which HCopy` ]; then
+  echo "[Notice] HTK is not installed, starting installation..."
+  if [ ! -e '/usr/X11' ]; then
+    echo "[Error] X11(XQuartz) is not installed. You can download from https://www.xquartz.org"
+    exit
+  fi
+  if [ ! -e 'HTK-3.4.1.tar.gz' ]; then
+    curl -u subdiox:o7Avmvdj http://htk.eng.cam.ac.uk/ftp/software/HTK-3.4.1.tar.gz -o HTK-3.4.1.tar.gz
+  fi
+  if [ -e 'htk' ]; then
+    rm -rf htk
+  fi
+  if [ ! -e '/Library/Developer/CommandLineTools' ]; then
+    echo "[Notice] Developer Tools are not installed. Starting installation..."
+    sudo xcode-select --install
+  fi
+  tar -xf HTK-3.4.1.tar.gz
+  cd htk
+  ./configure
+  make all LDFLAGS="-L/usr/X11/lib" CPPFLAGS="-I/usr/X11/include -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/malloc"
+  make install
+fi
 
-# 話者認識用
-bash ${bin_path}/setup/install4htk.sh
-sudo apt-get install libsvm-tools -y
-sudo apt-get install lib32std++6 -y
-sudo apt-get install python-numpy -y
-sudo apt-get install python3-numpy -y
+if [ `python3 -c "import numpy"` ]; then
+  echo "[Notice] numpy is not installed on your python3. Starting installation..."
+  pip3 install numpy
+fi
 
-# 音声合成
-#cd ${bin_path}/setup/source
-#bash install4syn.sh
-#cd ${bin_path}
+if [ ! `which svm-scale` ]; then
+  echo "[Notice] libsvm is not installed. Starting installation..."
+  brew install libsvm
+fi
 
-sudo apt-get install open-jtalk -y
-sudo apt-get install open-jtalk-mecab-naist-jdic -y
-sudo apt-get install hts-voice-nitech-jp-atr503-m001 -y
+if [ ! `which adinrec` ]; then
+  echo "[Notice] Julius is not installed. Starting installation..."
+  brew install julius julius-dictation-kit
+fi
